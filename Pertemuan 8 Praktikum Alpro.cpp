e66bac7959797e453af90c06b82b202d66e9e102
AddienGilang <114645629+AddienGilang@users.noreply.github.com>
2022-12-13 07:09:39 +0700

    Add files via upload

---
+++ Pertemuan 8 Praktikum Alpro.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+void tentukan_zodiak();
+// Deklarasi struktur untuk data zodiak
+struct zodiak
+{
+string nama;
+int tanggal_mulai;
+int tanggal_selesai;
+};
+
+// Deklarasi Variabel Global
+    const int jumlah_zodiak = 12;
+    zodiak daftar_zodiak[jumlah_zodiak] =
+    {
+    {"ARIES", 21, 3},
+    {"TAURUS", 21, 4},
+    {"GEMINI", 21, 5},
+    {"CANCER", 21, 6},
+    {"LEO", 21, 7},
+    {"VIRGO", 21, 8},
+    {"LIBRA", 21, 9},
+    {"SCORPIO", 21, 10},
+    {"SAGITTARIUS", 21, 11},
+    {"CAPRICORN", 21, 12},
+    {"AQUARIUS", 21, 1},
+    {"PISCES", 21, 2}
+    }; // Array untuk menyimpan data zodiak
+    int tanggal_lahir, bulan_lahir, tahun_lahir; // Variabel untuk menyimpan Tanggal, Bulan, dan Tahun Kelahiran
+
+int main(){
+
+    cout << " Masukkan tanggal lahir :" << endl;
+    cout << " Bulan lahir :"           << endl;
+    cout << " Tahun lahir :"           << endl;
+    cin >> tanggal_lahir >> bulan_lahir >> tahun_lahir;
+    tentukan_zodiak();
+    return 0;
+}
+// Fungsi untuk menentukan zodiak
+void tentukan_zodiak(){
+// Penentuan zodiak berdasarkan tanggal kelahiran
+for (int i = 0; i < jumlah_zodiak; i++)
+{
+    if (bulan_lahir == daftar_zodiak[i].tanggal_selesai)
+    {
+        if (tanggal_lahir >= daftar_zodiak[i].tanggal_mulai)
+        {
+            cout << "Zodiak Anda Adalah: " << daftar_zodiak[i].nama << endl;
+            break;
+        }
+        else if (i == 0)
+        {
+            cout << "Zodiak Anda Adalah: " << daftar_zodiak[11].nama << endl;
+            break;
+        }
+        else
+        {
+            cout << "Zodiak Anda Adalah: " << daftar_zodiak[i - 1].nama << endl;
+            break;
+        }
+    }
+}}
